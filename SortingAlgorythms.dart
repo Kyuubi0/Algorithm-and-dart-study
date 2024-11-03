99a1ba2a392a3692db42af1f110821ad7de68c4f
Kyuubi0 <uftsimge@gmail.com>
2024-11-04 01:31:51 +0300

    2
    shell sort

--- SortingAlgorythms.dart
+++ SortingAlgorythms.dart
@@ -1,16 +1,16 @@
-List<int> data=[10,7,4,9,5];
+List<int> data=[10,7,4,9,5,78,43,78,2,67,5];
 //best case O(n)
 //avarage case is O(n^2)
 //this algorythims analyze is O(n^2) so there it is guaranteed that this can't be bad than that
-void InsertionSort(List<int> data,n){
-  int i,j,tmp; 
+void InsertionSort(List<int> data,int n){
+  int i, j, tmp; 
   print(data);
-  for(i=0;i<=n;i++){
-    for(j=0;j<=n;j++){
-      if(data[i]<data[j]){
-          tmp=data[i];
-          data[i]=data[j];
-          data[j]=tmp;
+  for( i = 0 ; i < n ; i++ ){
+    for( j = 0 ; j < n ; j++ ){
+      if(data[i] < data[j]){
+          tmp = data[i];
+          data[i] = data[j];
+          data[j] = tmp;
       }
     }
   }
@@ -18,10 +18,25 @@ void InsertionSort(List<int> data,n){
   print(data);
 }
 
-void 
+void ShellSort(List<int> data,int n){
+  int i, j, tmp, gap;
+  print(data);
+  for( gap = n ~/ 2 ; gap > 0 ; gap = gap ~/ 2 ){
+    for( i = gap ; i < n ; i++ ){
+      tmp = data[i];
+      for( j = i ; j >= gap && data[j-gap] > tmp ; j -= gap ){
+        data[j] = data[j-gap];
+      }
+      data[j] = tmp;
+    }
+  }
+  print(data);
+}
+
 
 void main(){
-  int n=4;//n is the height
+  int n=11;//n is the height
+  ShellSort(data,n);
   InsertionSort(data,n);
   print(data);
 }
