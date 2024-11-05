ea8d502cb5fcf5180a1c3a3af96b1c3e58d033ff
Kyuubi0 <uftsimge@gmail.com>
2024-11-05 16:25:51 +0300

    bubble sort

--- SortingAlgorythms.dart
+++ SortingAlgorythms.dart
@@ -1,4 +1,5 @@
 List<int> data=[10,7,4,9,5,78,43,78,2,67,5];
+//heap sort is gonna be explaining in binary search trees
 //best case O(n)
 //avarage case is O(n^2)
 //this algorythims analyze is O(n^2) so there it is guaranteed that this can't be bad than that
@@ -19,7 +20,6 @@ void InsertionSort(List<int> data,int n){
 }
 
 //worst case O(n^2)
-//
 
 void ShellSort(List<int> data,int n){
   int i, j, tmp, gap;
@@ -37,9 +37,34 @@ void ShellSort(List<int> data,int n){
 }
 
 
+//best case is O(n)
+//worst and avarage case is O(n^2)
+void BubleSort(List<int> data,int n){
+  int i,j;
+  print(data);
+  for(i=0;i<n-1;i++){
+      for(j=0;j<n-i-1;j++){
+          if(data[j]>data[j+1]){
+            int tmp=data[j];
+            data[j]=data[j+1];
+            data[j+1]=tmp;
+          }
+      }
+  }
+  print(data);
+
+}
+
+void MergeSort(List<int> data,int n){
+  
+}
+
+
+
 void main(){
   int n=11;//n is the height
-  ShellSort(data,n);
-  InsertionSort(data,n);
-  print(data);
+  //BubleSort(data, n);
+  //ShellSort(data,n);
+  //InsertionSort(data,n);
+ // print(data);
 }
