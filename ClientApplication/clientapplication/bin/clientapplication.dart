210d02ede406f10d85176556c36448b581d25636
Kyuubi0 <uftsimge@gmail.com>
2024-11-10 19:27:15 +0300

    bipbopclient

---
+++ ClientApplication/clientapplication/bin/clientapplication.dart
@@ -0,0 +1,5 @@
+import 'package:clientapplication/clientapplication.dart' as clientapplication;
+
+void main(List<String> arguments) {
+  print('Hello world: ${clientapplication.calculate()}!');
+}
---
+++ ClientApplication/clientapplication/test/clientapplication_test.dart
@@ -0,0 +1,8 @@
+import 'package:clientapplication/clientapplication.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
