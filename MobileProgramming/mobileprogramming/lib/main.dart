1212b85c8bcd7762adad020ce1f5d4450dba1c0b
Kyuubi0 <uftsimge@gmail.com>
2024-11-21 21:10:45 +0300

    textWidget

--- MobileProgramming/mobileprogramming/lib/main.dart
+++ MobileProgramming/mobileprogramming/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +18,9 @@ class HomePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 233, 246, 248),
+      appBar: AppBar(
       title: Text("FirstApp"),
       actions: [
         IconButton(onPressed: () => {}, icon: Icon(Icons.save)),
@@ -25,7 +29,21 @@ class HomePage extends StatelessWidget{
         ]
       ),
       body: Center(
-        child: Text("Hello first app")
+        child: Text(
+          "Hello first app",
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.rtl,
+          style: TextStyle(
+            fontSize: 36,
+            color: const Color.fromARGB(255, 48, 175, 192),
+            fontWeight: FontWeight.bold,
+            //decoration: TextDecoration.underline,
+            wordSpacing: 10,
+            letterSpacing: 2,
+            fontStyle: FontStyle.italic,
+            //backgroundColor: const Color.fromARGB(255, 154, 211, 211),
+            ),
+        ),
       ),
     );
   }
