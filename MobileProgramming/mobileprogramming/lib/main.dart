1e02272cfee408a9a9f48882e3647ea3355f7444
Kyuubi0 <uftsimge@gmail.com>
2024-11-21 21:25:13 +0300

    TextDesign

--- MobileProgramming/mobileprogramming/lib/main.dart
+++ MobileProgramming/mobileprogramming/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -21,29 +23,67 @@ class HomePage extends StatelessWidget{
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 233, 246, 248),
       appBar: AppBar(
+      // ignore: prefer_const_constructors
       title: Text("FirstApp"),
       actions: [
         IconButton(onPressed: () => {}, icon: Icon(Icons.save)),
         IconButton(onPressed: () => {}, icon: Icon(Icons.delete)),
+        // ignore: prefer_const_constructors
         IconButton(onPressed: () => {}, icon: Icon(Icons.update)),
         ]
       ),
       body: Center(
-        child: Text(
-          "Hello first app",
-          textAlign: TextAlign.center,
-          textDirection: TextDirection.rtl,
-          style: TextStyle(
-            fontSize: 36,
-            color: const Color.fromARGB(255, 48, 175, 192),
-            fontWeight: FontWeight.bold,
-            //decoration: TextDecoration.underline,
-            wordSpacing: 10,
-            letterSpacing: 2,
-            fontStyle: FontStyle.italic,
-            //backgroundColor: const Color.fromARGB(255, 154, 211, 211),
+        child: RichText(
+          text: TextSpan(
+            text: "Rich Text is good for design \n",
+            style: TextStyle(
+              color: const Color.fromARGB(255, 45, 155, 245),
+              fontSize: 20,
+              fontStyle: FontStyle.italic,
             ),
+            children: [
+              TextSpan(
+                text: "Hello there!\n",
+                style: TextStyle(
+                color: const Color.fromARGB(255, 10, 173, 179),
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                ),
+              ),
+              TextSpan(
+                text: "Hello there!\n",
+                style: TextStyle(
+                color: const Color.fromARGB(255, 88, 146, 81),
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                ),
+              ),
+              TextSpan(
+                text: "Hello there!\n",
+                style: TextStyle(
+                color: const Color.fromARGB(255, 96, 101, 173),
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          ),
         ),
+        // Text(
+        //   "Hello first app",
+        //   textAlign: TextAlign.center,
+        //   textDirection: TextDirection.rtl,
+        //   style: TextStyle(
+        //     fontSize: 36,
+        //     color: const Color.fromARGB(255, 48, 175, 192),
+        //     fontWeight: FontWeight.bold,
+        //     //decoration: TextDecoration.underline,
+        //     wordSpacing: 10,
+        //     letterSpacing: 2,
+        //     fontStyle: FontStyle.italic,
+        //     //backgroundColor: const Color.fromARGB(255, 154, 211, 211),
+        //     ),
+        // ),
       ),
     );
   }
