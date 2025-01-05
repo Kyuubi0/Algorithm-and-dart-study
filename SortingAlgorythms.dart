934f03e07b6c404800b1aa6d5e826b4964c57289
Kyuubi0 <uftsimge@gmail.com>
2025-01-05 19:16:42 +0300

    RadixSort

--- SortingAlgorythms.dart
+++ SortingAlgorythms.dart
@@ -118,6 +118,50 @@ void MergeSort(List<int> data, int left, int right){
   }
 }
 
+void CountingSort(List<int> arr, int exp){
+  int n = arr.length;
+  List<int> output = List.filled(n,0); // output array
+  List<int> count = List.filled(10,0); // count array
+
+  // Store count of occurrences in count[]
+  for (int i = 0; i < n; i++){
+    count[(arr[i] ~/ exp) % 10]++;
+  }
+  // Change count[i] so that count[i] now contains actual
+  for (int i = 1; i < 10; i++){
+    count[i] += count[i - 1];
+  }
+
+  // Build the output array
+  for (int i = n - 1; i >= 0; i--){
+    output[count[(arr[i] ~/ exp) % 10] - 1] = arr[i];
+    count[(arr[i] ~/ exp) % 10]--;
+  }
+
+  // Copy the output array to arr[], so that arr[] now
+  // contains sorted numbers according to current digit
+  for (int i = 0; i < n; i++){
+    arr[i] = output[i];
+  }
+}
+
+void RadixSort(List<int> arr){
+  // Find the maximum number to know number of digits
+  int max = arr[0];
+  for (int i = 1; i < arr.length; i++){
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+
+  // Do counting sort for every digit. Note that
+  // instead of passing digit number, exp is passed.
+  // exp is 10^i where i is current digit number
+  for (int exp = 1; max ~/ exp > 0; exp *= 10){
+    CountingSort(arr, exp);
+  }
+}
+
 
 
 void main(){
